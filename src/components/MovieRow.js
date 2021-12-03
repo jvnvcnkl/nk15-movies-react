f2606f7ca0f3c739f021c7dfce3a77be78b50363
NikolaJ <jvnvcnk@gmail.com>
2021-12-03 21:55:33 +0100

    7th task

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,18 +1,31 @@
 import { useParams } from "react-router"
+import { useState } from "react";
+import { useEffect } from "react";
 
-
-export default function MovieRow({ prop }) {
+export default function MovieRow({ prop, callback }) {
     const movie = prop;
 
+    const [selected, setSelected] = useState(false);
+
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const newSelected = !selected;
+        if (selected) { setSelected(false) }
+        else { setSelected(true) }
+        callback(newSelected, movie.id)
+    }
     return (
-        <div>
+        <div
+            style={selected ? { marginBottom: '5px', border: '5px solid black' } : {}} >
             <h2>{movie.title}</h2>
             <p>Directed by {movie.director}</p>
             <p>Genre: {movie.genre}</p>
             <p>Duration: {movie.duration}</p>
             <p>Release date: {movie.release_date}</p>
             <img src={movie.image_url} />
-
+            <button onClick={(e) => handleClick(e)}>Select this movie</button>
+            <break></break>
         </div >
     )
 }
--- src/pages/AppMovies.js
+++ src/pages/AppMovies.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import {
     useDispatch, useSelector
 } from 'react-redux';
@@ -10,20 +10,53 @@ export default function AppMovies() {
 
     const dispatch = useDispatch();
 
+    const movies = useSelector(selectMovies);
+    const [selectedMovies, setSelectedMovies] = useState([
+    ])
 
-    const movies = useSelector(selectMovies)
-    console.log(movies)
 
     useEffect(() => {
         dispatch(getMovies());
     }, []);
 
+    const selectedMoviesCallback = (selected, movieId) => {
+
+        const index = selectedMovies.findIndex(object => object.id === movieId)
+        console.log(index)
+        if (index) {
+            const movieUpdate = { id: movieId, selected: selected }
+            console.log(movieUpdate)
+            setSelectedMovies([
+                ...selectedMovies.splice(index, 1),
+                movieUpdate
+            ])
+        }
+
+        else {
+
+            setSelectedMovies([
+                ...selectedMovies,
+                {
+                    id: movieId,
+                    selected: selected,
+                }
+            ])
+        }
+    }
+
+    useEffect(() => {
+        console.log(selectedMovies)
+    }, [selectedMovies])
+
+
+
     return (
         <div>
             <h1>Movies</h1>
+            Number of selected movies :{selectedMovies.length}
             <ul>
                 {movies.data.length ? movies.data.map(movie => (
-                    <li key={movie.id}><MovieRow prop={movie} /> </li>
+                    <li key={movie.id}><MovieRow prop={movie} callback={selectedMoviesCallback} /> </li>
 
                 )) : <p>No movies to show </p>}
             </ul>
